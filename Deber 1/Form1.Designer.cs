f9f09ff5af443eead9dd9f17e795676d7bf43488
Jjdavid90 <152727674+Jjdavid90@users.noreply.github.com>
2024-08-02 13:45:10 -0500

    David
    Deber

---
+++ Deber 1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Deber_1
+{
+    partial class Form_Tareas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst_Tareas = new System.Windows.Forms.ListBox();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.btn_Eliminar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_Materia = new System.Windows.Forms.TextBox();
+            this.txt_Tarea = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lst_Tareas
+            // 
+            this.lst_Tareas.FormattingEnabled = true;
+            this.lst_Tareas.ItemHeight = 20;
+            this.lst_Tareas.Location = new System.Drawing.Point(639, 128);
+            this.lst_Tareas.Name = "lst_Tareas";
+            this.lst_Tareas.Size = new System.Drawing.Size(511, 324);
+            this.lst_Tareas.TabIndex = 0;
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_Agregar.Location = new System.Drawing.Point(639, 75);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(134, 36);
+            this.btn_Agregar.TabIndex = 1;
+            this.btn_Agregar.Text = "Agregar Tarea";
+            this.btn_Agregar.UseVisualStyleBackColor = false;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // btn_Eliminar
+            // 
+            this.btn_Eliminar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_Eliminar.Location = new System.Drawing.Point(1054, 472);
+            this.btn_Eliminar.Name = "btn_Eliminar";
+            this.btn_Eliminar.Size = new System.Drawing.Size(96, 30);
+            this.btn_Eliminar.TabIndex = 2;
+            this.btn_Eliminar.Text = "Eliminar";
+            this.btn_Eliminar.UseVisualStyleBackColor = false;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(506, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Gestor de Tareas";
+            // 
+            // txt_Materia
+            // 
+            this.txt_Materia.Location = new System.Drawing.Point(174, 222);
+            this.txt_Materia.Name = "txt_Materia";
+            this.txt_Materia.Size = new System.Drawing.Size(293, 26);
+            this.txt_Materia.TabIndex = 4;
+            // 
+            // txt_Tarea
+            // 
+            this.txt_Tarea.Location = new System.Drawing.Point(74, 339);
+            this.txt_Tarea.Name = "txt_Tarea";
+            this.txt_Tarea.Size = new System.Drawing.Size(493, 26);
+            this.txt_Tarea.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Materia";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tarea";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 491);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Yo se que ustedes pueden =)";
+            // 
+            // Form_Tareas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Tarea);
+            this.Controls.Add(this.txt_Materia);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Eliminar);
+            this.Controls.Add(this.btn_Agregar);
+            this.Controls.Add(this.lst_Tareas);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form_Tareas";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_Tareas;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.Button btn_Eliminar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_Materia;
+        private System.Windows.Forms.TextBox txt_Tarea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
